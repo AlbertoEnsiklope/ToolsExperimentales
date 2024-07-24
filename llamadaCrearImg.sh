4a6822940f2442b6aa67251c5d454f9fd7e66177
AlbertoEnsiklope <makakitomakako69@gmail.com>
2024-07-24 04:01:23 +0200

    Update llamadaCrearImg.sh

--- llamadaCrearImg.sh
+++ llamadaCrearImg.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-# Instalar Pillow
-pip3 install pillow
+# Nombre del archivo a comprobar
+FILE="generar_imagenes.py"
 
-# Borrar imágenes previas
+# Comprobar si el archivo ya existe
+if [ -f "$FILE" ]; then
+    echo "$FILE ya existe. No se descargará de nuevo."
+else
+    echo "$FILE no encontrado. Descargando..."
+    curl -o $FILE https://raw.githubusercontent.com/AlbertoEnsiklope/ToolsExperimentales/main/generar_imagenes.py
+fi
+
+# Comprobar si Pillow está instalado
+if python3 -c "import PIL" &> /dev/null; then
+    echo "Pillow ya está instalado."
+else
+    echo "Pillow no está instalado. Instalando..."
+    pip3 install pillow
+fi
+
+# Vaciar Existentes
 rm -rf aleatoriedad
 
-# Ejecutar el script de Python
-python3 generar_imagenes.py
+# Ejecutar el script Python
+python3 $FILE
